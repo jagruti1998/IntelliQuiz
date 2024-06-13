a8fa74b2684960582d51331abb6317373e589144
Jagruti Goswami <jagrutigoswami402@gmail.com>
2024-06-14 01:32:04 +0530

    task 2

--- IntelliQuiz/src/main/java/com/app/IntelliQuiz/controller/QuestionController.java
+++ IntelliQuiz/src/main/java/com/app/IntelliQuiz/controller/QuestionController.java
@@ -5,6 +5,7 @@ import com.app.IntelliQuiz.entity.Question;
 import com.app.IntelliQuiz.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,12 @@ public class QuestionController {
     public List<Question> getAllQuestions(){
 
         return questionService.getAllQuestions();
+    }
+
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+
     }
 }
